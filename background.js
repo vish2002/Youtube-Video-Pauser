4b04f353a0cd3e9ae9750a873f6a2b9d24cddbee
Vishal Kurve <vishalkurve7@gmail.com>
2024-06-09 13:41:02 +0530

    Update background.js
    updated code

--- background.js
+++ background.js
@@ -1,42 +1,30 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ enabled: true });
-});
-
 chrome.tabs.onActivated.addListener(activeInfo => {
-  chrome.storage.local.get('enabled', data => {
-    if (!data.enabled) return;
-
-    chrome.tabs.get(activeInfo.tabId, tab => {
-      if (tab.url.includes("youtube.com")) {
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          function: resumeYouTubeVideo
+  chrome.tabs.get(activeInfo.tabId, tab => {
+    if (tab.url.includes("youtube.com")) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: resumeYouTubeVideo
+      });
+    } else {
+      chrome.tabs.query({}, tabs => {
+        tabs.forEach(tab => {
+          if (tab.url.includes("youtube.com")) {
+            chrome.scripting.executeScript({
+              target: { tabId: tab.id },
+              function: pauseYouTubeVideo
+            });
+          }
         });
-      } else {
-        chrome.tabs.query({}, tabs => {
-          tabs.forEach(tab => {
-            if (tab.url.includes("youtube.com")) {
-              chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: pauseYouTubeVideo
-              });
-            }
-          });
-        });
-      }
-    });
+      });
+    }
   });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url.includes("youtube.com")) {
-    chrome.storage.local.get('enabled', data => {
-      if (!data.enabled) return;
-
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        function: resumeYouTubeVideo
-      });
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: resumeYouTubeVideo
     });
   }
 });
